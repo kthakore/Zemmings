da23d7fc53581a0700b86e8db79bff1d5e4cc1a8
Shawn M Moore <code@sartak.org>
2012-06-14 23:57:37 -0500

    Create a protagonist body, have animation match it

--- lib/Zemmings/Level.pm
+++ lib/Zemmings/Level.pm
@@ -8,11 +8,25 @@ my $sky_rect = rect( 0, 0, app->w, app->h);
 
 my $floor = world->create_body( x => app->w / 2, y => 50, w => app->w, h => 100 );
 
+my $protagonist = world->create_body(
+        type => 'dynamic',
+        x    => 50,
+        y    => 150,
+        w    => 32,
+        h    => 32,
+        friction => 0.0, # optional
+        density  => 0.7, # optional
+);
+
 my $animation = SDLx::Sprite::Animated->new(image => 'share/stickzombie.png', width => 32, height => 32);
 $animation->start;
 show {
     app->draw_rect( $sky_rect, 0x3BB9FFFF );
     app->draw_rect( $floor->rect, [0, 200, 0, 255] );
+
+    # have the animation track the protagonist
+    $animation->x($protagonist->x);
+    $animation->y(app->h - $protagonist->y);
     $animation->draw(app);
 
     app->update;
