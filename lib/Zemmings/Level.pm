8c72ddde4ac0ba254aa6274fb8ed2ef4b77d62d6
Shawn M Moore <code@sartak.org>
2012-06-14 23:33:14 -0500

    Fix animation yay

--- lib/Zemmings/Level.pm
+++ lib/Zemmings/Level.pm
@@ -5,10 +5,11 @@ use SDLx::Sprite::Animated;
 update { world->update };
 
 my $app_rect = rect( 0, 0, app->w, app->h);
-# my $animation = SDLx::Sprite::Animated->new(image => 'share/stickzombie.png');
+my $animation = SDLx::Sprite::Animated->new(image => 'share/stickzombie.png', width => 32, height => 32);
+$animation->start;
 show {
     app->draw_rect( $app_rect, 0x3BB9FFFF );
-    # app->draw_rect( $animation->rect, 0xFF000000 );
+    $animation->draw(app);
 
     app->update;
 };
