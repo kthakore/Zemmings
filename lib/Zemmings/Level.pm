a03a70433f2943be449fefc8d90cb10a589fea5e
Shawn M Moore <code@sartak.org>
2012-06-14 23:45:47 -0500

    app_rect -> sky_rect

--- lib/Zemmings/Level.pm
+++ lib/Zemmings/Level.pm
@@ -4,14 +4,14 @@ use SDLx::Sprite::Animated;
 
 update { world->update };
 
-my $app_rect = rect( 0, 0, app->w, app->h);
+my $sky_rect = rect( 0, 0, app->w, app->h);
 
 my $floor = world->create_body( x => app->w / 2, y => 50, w => app->w, h => 100 );
 
 my $animation = SDLx::Sprite::Animated->new(image => 'share/stickzombie.png', width => 32, height => 32);
 $animation->start;
 show {
-    app->draw_rect( $app_rect, 0x3BB9FFFF );
+    app->draw_rect( $sky_rect, 0x3BB9FFFF );
     app->draw_rect( $floor->rect, [0, 200, 0, 255] );
     $animation->draw(app);
 
