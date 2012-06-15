2758e17adae19b455eec23d1a19fb59759e60bd9
Kartik Thakore <thakore.kartik@gmail.com>
2012-06-15 03:11:27 -0400

    Getting back perl object from fixture contact of collision

--- lib/Zemmings.pm
+++ lib/Zemmings.pm
@@ -1,12 +1,16 @@
 package Zemmings;
 use Avenger;
 use Zemmings::Map;
-
+use Data::Dumper; 
 ### THIS NEEDS TO GO TO AVENGER
    my $listener = Box2D::PerlContactListener->new();
     world->{world}->SetContactListener( $listener );
-    $listener->SetPostSolveSub( sub { my $contact= shift; my $c_impulse = shift; 
-                                        warn $contact
+    $listener->SetPostSolveSub( sub { 
+                                    my $contact= shift; my $c_impulse = shift; 
+                                       my $a_b_a = $contact->GetFixtureA()->GetUserData();       
+                                       my $a_b_b = $contact->GetFixtureB()->GetUserData();       
+                                        warn Dumper $a_b_a;                      
+
                                     }  );
 
 
