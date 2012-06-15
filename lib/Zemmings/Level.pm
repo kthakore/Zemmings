9a5d266b6f068fa856d36aa7bba6f753bedea0e1
Brock Wilcox <awwaiid@thelackthereof.org>
2012-06-15 00:00:00 -0400

    Add zombie sprite

--- lib/Zemmings/Level.pm
+++ lib/Zemmings/Level.pm
@@ -1,11 +1,14 @@
 package Zemmings::Level;
 use Avenger;
+use SDLx::Sprite::Animated;
 
 update { world->update };
 
 my $app_rect = rect( 0, 0, app->w, app->h);
+# my $animation = SDLx::Sprite::Animated->new(image => 'share/stickzombie.png');
 show {
     app->draw_rect( $app_rect, 0x3BB9FFFF );
+    # app->draw_rect( $animation->rect, 0xFF000000 );
 
     app->update;
 };
