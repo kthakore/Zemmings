0497a1c44f182290c3e5e781a82ca5952b8a162c
Kartik Thakore <thakore.kartik@gmail.com>
2012-06-14 20:22:49 -0400

    Added level format

--- lib/Zemmings/Level.pm
+++ lib/Zemmings/Level.pm
@@ -1,5 +1,38 @@
 package Zemmings::Level;
-#Is the zemming level 
+use Avenger;
 
+#Is the zemming level screen
+
+#Single Zemming Actor
+actor 'Zemming' => {
+    body => {
+        x        => 10,
+        y        => 600,
+        velocity => { x => 5, y => 0 }
+    }
+};
+
+#Loading JSON Actors
+#actors => 'level1.json'
+#Looks for actors arrayref[hash] in data/level1.json
+
+error: src refspec levels does not match any.
+error: failed to push some refs to 'git@github.com:kthakore/Zemmings.git'
+    level  => 1,
+    points => 500,
+
+    # ...
+
+};
+
+update {
+
+};
+
+show {
+
+};
+
+ 
 
 1;
