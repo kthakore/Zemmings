9363136788897fcf5094d43704b235cd4d185111
Kartik Thakore <thakore.kartik@gmail.com>
2013-01-25 17:49:23 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Old branch was over optimized. Starting as simple as possible again, while writing chapters.
---
+++ zemmings.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use SDLx::App;
+
+my $app = SDLx::App->new( eoq => 1, delay => 10 );
+
+$app->run();
